c94def582984b47bf7e6b93b9d4169640b337857
mwojtacz <mwojtacz@akamai.com>
2022-02-22 16:31:31 +0100

    Init

---
+++ src/main/java/com/maciej/wojtaczka/messagebox/MessageBoxApplication.java
@@ -0,0 +1,13 @@
+package com.maciej.wojtaczka.messagebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageBoxApplication.class, args);
+    }
+
+}
